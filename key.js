72f7f2f5d97a7c762a0895bdec4432b79cd5ceb7
thayes3116 <timhayes3116@gmail.com>
2017-06-15 15:39:05 -0400

    initial set up

---
+++ key.js
@@ -0,0 +1,12 @@
+```JavaScript
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<	kTXJXtruuoYaa3C0uVBnr68zs>',
+  consumer_secret: '<	0Dwknokn9lCQlVH9xFyECruJExBFLiR4pE65wdvvSnugu1xxLR
+>',
+  access_token_key: '<875390727076892672-h4dCc4S2TRBWB3thZe9qCbyausAyqOT>',
+  access_token_secret: '<8Rpy2oNjNTp5MMjfqjgxXtsmtAmNZTcedY9eVi62Lx04K
+>',
+}
+```
